16d0c84a2df25e5087b394eefbd6930b87187465
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-04-23 10:38:28 -0700

    update tests

---
+++ Sources/SPFKTesting/Tags.swift
@@ -0,0 +1,6 @@
+import Testing
+
+extension Tag {
+    /// A tag to indicate this is a development test rather than functionality that should be unit tested
+    @Tag public static var development: Self
+}
