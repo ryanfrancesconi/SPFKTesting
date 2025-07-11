aeb33fb0617b4aab7b008464fc334b0231602b69
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-07-10 17:04:45 -0700

    audio file type refactoring

--- Sources/SPFKTesting/Tags+.swift
+++ Sources/SPFKTesting/Tags+.swift
@@ -3,4 +3,5 @@ import Testing
 extension Tag {
     /// A tag to indicate this is a development test rather than functionality that should be unit tested
     @Tag public static var development: Self
+    @Tag public static var file: Self
 }
