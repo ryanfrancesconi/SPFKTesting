4269cca569f6a01dd2de32857866f32a83c139d4
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-07-11 14:57:23 -0700

    add support for flac/ogg

--- Sources/SPFKTesting/BundleResources.swift
+++ Sources/SPFKTesting/BundleResources.swift
@@ -17,9 +17,23 @@ public class BundleResources: AnyObject {
     public func resource(named name: String) -> URL {
         resourcesDirectory.appendingPathComponent(name)
     }
-    
+
     public var audioCases: [URL] {
         [mp3_id3, wav_bext_v2, tabla_mp4, tabla_wav, tabla_6_channel, cowbell_wav, pink_noise]
+    }
+
+    public var formats: [URL] {
+        [
+            tabla_aac,
+            tabla_aif,
+            // tabla_caf,
+            tabla_flac,
+            tabla_m4a,
+            tabla_mp3,
+            tabla_mp4,
+            tabla_ogg,
+            tabla_wav,
+        ]
     }
 }
 
@@ -34,6 +48,10 @@ extension BundleResources {
         resource(named: "and-oh-how-they-danced.wav")
     }
 
+    public var tabla_aac: URL {
+        resource(named: "tabla.aac")
+    }
+
     public var tabla_mp4: URL {
         resource(named: "tabla.mp4")
     }
@@ -41,7 +59,31 @@ extension BundleResources {
     public var tabla_wav: URL {
         resource(named: "tabla.wav")
     }
-    
+
+    public var tabla_flac: URL {
+        resource(named: "tabla.flac")
+    }
+
+    public var tabla_ogg: URL {
+        resource(named: "tabla.ogg")
+    }
+
+    public var tabla_aif: URL {
+        resource(named: "tabla.aif")
+    }
+
+    public var tabla_caf: URL {
+        resource(named: "tabla.caf")
+    }
+
+    public var tabla_mp3: URL {
+        resource(named: "tabla.mp3")
+    }
+
+    public var tabla_m4a: URL {
+        resource(named: "tabla.m4a")
+    }
+
     public var tabla_6_channel: URL {
         resource(named: "tabla_6_channel.wav")
     }
@@ -65,5 +107,4 @@ extension BundleResources {
     public var no_data_chunk: URL {
         resource(named: "no_data_chunk.wav")
     }
-    
 }
