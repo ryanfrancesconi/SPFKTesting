9db880f82eada120408a08549a39a876d96b9a3d
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-11-24 18:06:12 -0800

    revert package.swift

--- Package.swift
+++ Package.swift
@@ -2,22 +2,13 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-import Foundation
 
 // Swift target
 private let name: String = "SPFKTesting"
-private let dependencyNames: [String] = []
-private let dependencyBranch = "main"
-private let useLocalDependencies: Bool = false
+
 private let platforms: [PackageDescription.SupportedPlatform]? = [
-    .macOS(.v12),
-    .iOS(.v15),
+    .macOS(.v12)
 ]
-
-// MARK: - Reusable Code for single package
-
-private let nameTests: String = "\(name)Tests" // Test target
-private let githubBase = "https://github.com/ryanfrancesconi"
 
 private let products: [PackageDescription.Product] = [
     .library(
@@ -26,68 +17,22 @@ private let products: [PackageDescription.Product] = [
     )
 ]
 
-private var packageDependencies: [PackageDescription.Package.Dependency] {
-     let local: [PackageDescription.Package.Dependency] =
-        dependencyNames.map {
-            .package(name: "\($0)", path: "../\($0)")
-        }
-
-        
-     let remote: [PackageDescription.Package.Dependency] =
-        dependencyNames.map {
-            .package(url: "\(githubBase)/\($0)", branch: dependencyBranch)
-        }
-    
-    return useLocalDependencies ? local : remote
-}
-
-// is there a Sources/[NAME]/Resources folder?
-private var swiftTargetResources: [PackageDescription.Resource]? {
-    // package folder
-    let root = URL(fileURLWithPath: #file).deletingLastPathComponent()
-    
-    let dir = root.appending(component: "Sources")
-        .appending(component: name)
-        .appending(component: "Resources")
-    
-    let exists = FileManager.default.fileExists(atPath: dir.path)
-    
-    return exists ? [.process("Resources")] : nil
-}
-
-private var swiftTargetDependencies: [PackageDescription.Target.Dependency] {
-    let names = dependencyNames.filter { $0 != "SPFKTesting" }
-    
-    return names.map {
-        .byNameItem(name: "\($0)", condition: nil)
-    }
-}
-
-private let swiftTarget: PackageDescription.Target = .target(
-    name: name,
-    dependencies: swiftTargetDependencies,
-    resources: swiftTargetResources
-)
-
-private var testTargetDependencies: [PackageDescription.Target.Dependency] {
-    var array: [PackageDescription.Target.Dependency] = [
-        .byNameItem(name: name, condition: nil),
-    ]
-
-    if dependencyNames.contains("SPFKTesting") {
-        array.append(.byNameItem(name: "SPFKTesting", condition: nil))
-    }
-    
-    return array
-}
-
-private let testTarget: PackageDescription.Target = .testTarget(
-    name: nameTests,
-    dependencies: testTargetDependencies
-)
-
 private let targets: [PackageDescription.Target] = [
-    swiftTarget, testTarget
+    // Swift
+    .target(
+        name: name,
+        dependencies: [],
+        resources: [
+            .process("Resources") // all test files should be in here
+        ]
+    ),
+
+    .testTarget(
+        name: "\(name)Tests",
+        dependencies: [
+            .byNameItem(name: name, condition: nil),
+        ]
+    )
 ]
 
 let package = Package(
@@ -95,8 +40,6 @@ let package = Package(
     defaultLocalization: "en",
     platforms: platforms,
     products: products,
-    dependencies: packageDependencies,
     targets: targets,
     cxxLanguageStandard: .cxx20
 )
-
